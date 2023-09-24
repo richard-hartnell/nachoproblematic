f0f8b8fd2d8b9fee54b6a135921b90d097c12f97
richard-hartnell <richard@richardhartnell.com>
2023-09-23 22:33:38 -0700

    Nacho-pulldown section exists

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const toplogo = document.getElementById('toplogo');
 document.getElementById('toplogo').addEventListener("click", pullTopBar)
 
 function pullTopBar() {
-    document.getElementById('topbar').style.height = '80%';
+    document.getElementById('topbar').style.transition = '0.75s';
+    document.getElementById('topbar').style.height = '70%';
+    document.getElementById('toplogo').style.top = '90%';
+    document.getElementById('toplogo').style.paddingTop = '2vw';
 }
