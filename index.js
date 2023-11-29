f25fb20ac0e304561fe76ad9c47ed160e19b027d
richard-hartnell <richard@richardhartnell.com>
2023-11-28 21:47:24 -0800

    Animation fixes, added textures

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const toplogo = document.getElementById('toplogo');
 document.getElementById('toplogo').addEventListener("click", pullTopBar)
 
 function pullTopBar() {
-    document.getElementById('topbar').style.transition = '0.75s';
-    document.getElementById('topbar').style.height = '70%';
-    document.getElementById('toplogo').style.top = '90%';
-    document.getElementById('toplogo').style.paddingTop = '2vw';
+    document.getElementById('hand').style = "display: none";
+    const theTop = document.getElementById('topbar');
+    theTop.style.backgroundImage = "purple";
+    theTop.style.transition = '0.75s';
+    theTop.style.height = '70%';
+    document.getElementById('toplogo').style.top = '98%';
+    // document.getElementById('toplogo').style.paddingTop = '4vw';
+    document.getElementById('nacho-rule').classList.add('fadein')
 }
