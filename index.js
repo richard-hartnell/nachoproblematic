8f1f7fc812eb3eb8a7f81ed6bc67e2fc54c3030e
richard-hartnell <richard@richardhartnell.com>
2024-01-31 23:15:19 -0800

    nacho pull fixes.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const topbar = document.getElementById('topbar');
+const theTop = document.getElementById('topbar');
+const surprise = document.getElementById('nacho-surprise');
 const toplogo = document.getElementById('toplogo');
 document.getElementById('toplogo').addEventListener("click", pullTopBar)
 
 function pullTopBar() {
     document.getElementById('hand').style = "display: none";
-    const theTop = document.getElementById('topbar');
     theTop.style.backgroundImage = "purple";
     theTop.style.transition = '0.75s';
     theTop.style.height = '70%';
     document.getElementById('toplogo').style.top = '98%';
     // document.getElementById('toplogo').style.paddingTop = '4vw';
-    document.getElementById('nacho-rule').classList.add('fadein')
+    document.getElementById('nacho-rule').classList.add('fadein');
+    setTimeout(100);
+    document.getElementById('surprise').classList.add('fadein');
+    surprise.style.display = 'block';
+
 }
