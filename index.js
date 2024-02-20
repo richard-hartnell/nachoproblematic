66506637defac800d44b4e44d8a14f73b5411dec
Richard Hartnell <109207464+richard-hartnell@users.noreply.github.com>
2024-02-20 10:18:45 -0800

    More top bar development (compbook lightning bolt)

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const theTop = document.getElementById('topbar');
 const surprise = document.getElementById('surprise');
+const surprisetop = document.getElementById('surprise-top');
 const toplogo = document.getElementById('toplogo');
 document.getElementById('toplogo').addEventListener("click", pullTopBar)
 
 function pullTopBar() {
     document.getElementById('hand').style = "display: none";
     theTop.style.transition = '0.75s';
-    theTop.style.height = '70%'; // this is the next fix.
+    theTop.style.height = '70%';
     theTop.style.bottom = '30%';
-    toplogo.style.bottom = '-98%'; // this is the next fix. change this to bottom so it aligns with #surprise
+    toplogo.style.bottom = '-98%';
     surprise.style.transition = '0.75s';
     surprise.style.bottom = '30vh';
+    surprisetop.style.top = '0px';
     document.getElementById('nacho-rule').classList.add('fadein');
-    
 }
