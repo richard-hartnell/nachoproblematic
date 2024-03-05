eee090fae914b5bd1ae19019b39323b864ef09a4
Richard Hartnell <109207464+richard-hartnell@users.noreply.github.com>
2024-03-05 12:34:43 -0800

    Fixed hand, top margin

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ document.getElementById('toplogo').addEventListener("click", pullTopBar)
 function pullTopBar() {
     document.getElementById('hand').style = "display: none";
     theTop.style.transition = '0.75s';
-    theTop.style.height = '70%';
+    theTop.style.height = '75%';
     theTop.style.bottom = '30%';
     toplogo.style.bottom = '-98%';
     surprise.style.transition = '0.75s';
     surprise.style.bottom = '30vh';
-    surprisetop.style.top = '0px';
+    surprisetop.style.height = '100%';
     document.getElementById('nacho-rule').classList.add('fadein');
 }
