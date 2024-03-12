b04e58ae0fef153bd2c0d7764a0a119521577eac
richard-hartnell <richard@richardhartnell.com>
2024-03-11 21:45:33 -0700

    Pre-release chaos but we're getting there.

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const theTop = document.getElementById('topbar');
 const surprise = document.getElementById('surprise');
 const surprisetop = document.getElementById('surprise-top');
 const toplogo = document.getElementById('toplogo');
-document.getElementById('toplogo').addEventListener("click", pullTopBar)
+document.getElementById('toplogo').addEventListener("click", toggleTopBar)
+
+function toggleTopBar() {
+    if (theTop.style.height === '40px') {
+        pullTopBar();
+    } else {
+        retractTopBarMenu();
+    }
+}
 
 function pullTopBar() {
     document.getElementById('hand').style = "display: none";
@@ -12,6 +20,19 @@ function pullTopBar() {
     toplogo.style.bottom = '-98%';
     surprise.style.transition = '0.75s';
     surprise.style.bottom = '30%';
+    surprisetop.style.height = '100%';
+    document.getElementById('nacho-rule').classList.add('fadein');
+}
+
+function retractTopBarMenu() {
+    document.getElementById('hand').style = "display: block";
+    theTop.style.transition = '0.75s';
+    toplogo.style.transition = '0.75s';
+    theTop.style.height = '40px';
+    theTop.style.bottom = 'initial';
+    toplogo.style.bottom = 'initial';
+    surprise.style.transition = '0.75s';
+    surprise.style.bottom = '130vh';
     surprisetop.style.height = '100%';
     document.getElementById('nacho-rule').classList.add('fadein');
 }
